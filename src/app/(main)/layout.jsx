3247106472111bbbe5240214c7eda31cc527960f
SadmanG <sadmangergees@gmail.com>
2026-05-01 11:41:44 +0600

    Fixing Navbar

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,10 +1,8 @@
-import Header from "@/components/shared/Header";
 import Navbar from "@/components/shared/Navbar";
 
 const MainLayout = ({children}) => {
     return (
         <>
-            <Header></Header>
             <Navbar></Navbar>
             <main>{children}</main>
         </>
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -4,32 +4,77 @@ import Link from 'next/link';
 import React from 'react';
 import userAvatar from '@/assets/user.png';
 import NavLink from './NavLink';
+import { FaGoogle } from 'react-icons/fa';
 import { authClient } from "@/lib/auth-client"
 
 const Navbar = () => {
+    const handleGoogleSignIn = async () => {
+        const data = await authClient.signIn.social({
+            provider: "google",
+        });
+    };
     const { data: session, isPending } = authClient.useSession();
     const user = session?.user;
     return (
-        <div className='container mx-auto flex justify-between gap-4 mt-6'>
-            <div></div>
-            <ul className='flex justify-between items-center text-gray-700 gap-3'>
-                <li><NavLink href={'/'}>Home</NavLink></li>
-                <li><NavLink href={'/about'}>About</NavLink></li>
-                <li><NavLink href={'/career'}>Career</NavLink></li>
-            </ul>
-            {isPending ? (<span className="loading loading-spinner loading-lg"></span>) : user ? (<div className='flex items-center gap-2'>
-                <h2>Hello, {user.name}</h2>
-                <Image src={user.image || userAvatar} alt="User Avatar" width={60} height={60}></Image>
-                <button className='btn mr-4 bg-red-500 text-white' onClick={async() => await authClient.signOut()}>Logout</button>
-            </div>) :
-            (
-            <div className='flex'>
-                <Link href={'/register'}><button className='btn mr-4 bg-blue-500 text-white'>Register</button></Link>
-                <Link href={'/login'}><button className='btn mr-4 bg-green-500 text-white'>Login</button></Link>
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><NavLink href={'/'}>Home</NavLink></li>
+                        <li><NavLink href={'/about'}>About</NavLink></li>
+                        <li><NavLink href={'/career'}>Career</NavLink></li>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">SunCart</a>
             </div>
-            )}
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><NavLink href={'/'}>Home</NavLink></li>
+                    <li><NavLink href={'/about'}>About</NavLink></li>
+                    <li><NavLink href={'/career'}>Career</NavLink></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                {isPending ? (<span className="loading loading-spinner loading-lg"></span>) : user ? (<div className='flex items-center gap-2'>
+                    <h2>Hello, {user.name}</h2>
+                    <Image src={user.image || userAvatar} alt="User Avatar" width={60} height={60}></Image>
+                    <button className='btn mr-4 bg-red-500 text-white' onClick={async () => await authClient.signOut()}>Logout</button>
+                </div>) :
+                    (
+                        <div className='flex'>
+                            <Link href={'/register'}><button className='btn mr-4 bg-blue-500 text-white'>Register</button></Link>
+                            <Link href={'/login'}><button className='btn mr-4 bg-green-500 text-white'>Login</button></Link>
+                            <button className='btn border-blue-500 text-blue-500' onClick={handleGoogleSignIn}><FaGoogle /> Login with Google</button>
+                        </div>
+                    )}
+            </div>
         </div>
     );
 };
 
 export default Navbar;
+
+{/* <div className='container mx-auto flex justify-between gap-4 mt-6'>
+    <div></div>
+    <ul className='flex justify-between items-center text-gray-700 gap-3'>
+        <li><NavLink href={'/'}>Home</NavLink></li>
+        <li><NavLink href={'/about'}>About</NavLink></li>
+        <li><NavLink href={'/career'}>Career</NavLink></li>
+    </ul>
+    {isPending ? (<span className="loading loading-spinner loading-lg"></span>) : user ? (<div className='flex items-center gap-2'>
+        <h2>Hello, {user.name}</h2>
+        <Image src={user.image || userAvatar} alt="User Avatar" width={60} height={60}></Image>
+        <button className='btn mr-4 bg-red-500 text-white' onClick={async () => await authClient.signOut()}>Logout</button>
+    </div>) :
+        (
+            <div className='flex'>
+                <Link href={'/register'}><button className='btn mr-4 bg-blue-500 text-white'>Register</button></Link>
+                <Link href={'/login'}><button className='btn mr-4 bg-green-500 text-white'>Login</button></Link>
+            </div>
+        )}
+</div> */}
