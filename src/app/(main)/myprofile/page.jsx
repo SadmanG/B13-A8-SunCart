cfb7be778a9ecfa991ff083c4cfede7a14881eb2
SadmanG <sadmangergees@gmail.com>
2026-05-03 19:17:19 +0600

    Fixing MyProfile

--- src/app/(main)/myprofile/page.jsx
+++ src/app/(main)/myprofile/page.jsx
@@ -1,12 +1,28 @@
-export const metadata = {
-  title: "SunCart | My Profile",
-  description: "Profile Page",
-};
+'use client'
+import { authClient } from "@/lib/auth-client"
+import userAvatar from '@/assets/user.png';
+import Image from "next/image";
 
 const MyProfilePage = () => {
+    const { data: session, isPending } = authClient.useSession();
+    const user = session?.user;
     return (
-        <div>
-            See Your Profile!
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row gap-10">
+                <div className="relative w-full max-w-md aspect-square overflow-hidden rounded-lg shadow-2xl">
+                    <Image
+                        src={user.image || userAvatar}
+                        alt={user.name}
+                        fill
+                        className="object-cover"
+                        priority
+                    />
+                </div>
+                <div>
+                    <h1 className="text-5xl font-bold">UserName: {user.name}</h1>                   
+                    <button className="btn btn-info">Update Information</button>
+                </div>
+            </div>
         </div>
     );
 };
